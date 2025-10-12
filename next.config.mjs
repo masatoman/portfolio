898f1c71ed9741a48d2233003bf206a8e48f6634
masatoman <kanon02sky@gmail.com>
2025-10-12 10:24:23 +0900

    [修正] ビルドエラーを解決：optimizeCssを無効化

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,6 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   experimental: {
-    optimizeCss: true,
     optimizePackageImports: ['lucide-react', '@radix-ui/react-icons'],
   },
   compiler: {
